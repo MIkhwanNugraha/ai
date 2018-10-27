fe7c95bbe6f03aa62cdada544a0e0f0e0b915f9e
MikhwanNugraha <mikhwannugraha@yahoo.co.id>
2018-10-27 16:31:45 +0400

    versi 100

---
+++ app/src/main/java/com/dicoding/associate/smslistenerapp/DownloadService.java
@@ -0,0 +1,66 @@
+package com.dicoding.associate.smslistenerapp;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.util.Log;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class DownloadService extends IntentService {
+    // TODO: Rename actions, choose action names that describe tasks that this
+    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+    public static final String TAG = "DownloadService";
+
+    public DownloadService() {
+        super("DownloadService");
+    }
+
+    /**
+     * Starts this service to perform action Foo with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+
+
+    /**
+     * Starts this service to perform action Baz with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d(TAG, "Download Service dijalankan");
+        if (intent != null) {
+            try {
+                Thread.sleep(5000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            Intent notifyFinishIntent = new Intent(MainActivity.ACTION_DOWNLOAD_STATUS);
+            sendBroadcast(notifyFinishIntent);
+        }
+    }
+
+    /**
+     * Handle action Foo in the provided background thread with the provided
+     * parameters.
+     */
+
+
+    /**
+     * Handle action Baz in the provided background thread with the provided
+     * parameters.
+     */
+
+}
